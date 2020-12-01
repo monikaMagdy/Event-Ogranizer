ce6551bd4978431a098d3eeb23e369ee529388ec
Madonna <madonna@Marcoss-MacBook-Pro.local>
2020-12-01 14:47:36 +0200

    edit favorite interaction

--- lib/preview_events.dart
+++ lib/preview_events.dart
@@ -33,7 +33,6 @@ class _DisplayEvents extends State<DisplayEvents> {
       ),
       body: Column(
         children: eventData.eventDB.map((data) {
-          final bool alreadySaved = eventData.eventDB.contains(data.isFavorite);
           return ListTile(
             title: Text(data.eventName),
             subtitle: Column(
@@ -45,15 +44,7 @@ class _DisplayEvents extends State<DisplayEvents> {
                 Text(data.limitAttending.toString()),
               ],
             ),
-            trailing: Icon(
-              data.isFavorite ? Icons.favorite : Icons.favorite_border,
-              color: data.isFavorite ? Colors.red : null,
-            ),
-            onTap: () {
-              setState(() {
-                data.setFavorite(!data.isFavorite);
-              });
-            },
+            trailing: FavoriteWidget(),
           );
         }).toList(),
       ),
@@ -61,47 +52,47 @@ class _DisplayEvents extends State<DisplayEvents> {
   }
 }
 
-// class FavoriteWidget extends StatefulWidget {
-//   _FavoriteWidget createState() => _FavoriteWidget();
-// }
+class FavoriteWidget extends StatefulWidget {
+  _FavoriteWidget createState() => _FavoriteWidget();
+}
 
-// class _FavoriteWidget extends State<FavoriteWidget> {
-//   @override
-//   Widget build(BuildContext context) {
-//     bool _isFavorited = true;
-//     int _favoriteCount = 41;
-//     void _toggleFavorite() {
-//       setState(() {
-//         if (_isFavorited) {
-//           _favoriteCount -= 1;
-//           _isFavorited = false;
-//         } else {
-//           _favoriteCount += 1;
-//           _isFavorited = true;
-//         }
-//       });
-//     }
+class _FavoriteWidget extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
 
-//     return Row(
-//       mainAxisSize: MainAxisSize.min,
-//       children: [
-//         Container(
-//           padding: EdgeInsets.all(0),
-//           child: IconButton(
-//             padding: EdgeInsets.all(0),
-//             alignment: Alignment.centerRight,
-//             icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
-//             color: _isFavorited ? Colors.red : null,
-//             onPressed: _toggleFavorite,
-//           ),
-//         ),
-//         SizedBox(
-//           width: 18,
-//           child: Container(
-//             child: Text('$_favoriteCount'),
-//           ),
-//         ),
-//       ],
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            padding: EdgeInsets.all(0),
+            alignment: Alignment.centerRight,
+            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: _isFavorited ? Colors.red : null,
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favoriteCount'),
+          ),
+        ),
+      ],
+    );
+  }
+}
