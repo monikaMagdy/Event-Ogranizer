9fb6b0b422d76d7e66c1342ae3d7ace211009196
Madonna <madonna@Marcoss-MacBook-Pro.local>
2020-12-01 21:50:01 +0200

    set eventData into setstate to save list action

--- lib/preview_events.dart
+++ lib/preview_events.dart
@@ -22,17 +22,29 @@ class DisplayEvents extends StatefulWidget {
 }
 
 class _DisplayEvents extends State<DisplayEvents> {
-  Event event;
+  EventData eventData = new EventData();
+
+  List<Event> list;
+
+  @override
+  void initState() {
+    list = eventData.eventDB;
+  }
+
+  void refresh() {
+    setState(() {
+      list = eventData.eventDB;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    EventData eventData = new EventData();
     return Scaffold(
       appBar: AppBar(
         title: Text("Display events"),
       ),
       body: Column(
-        children: eventData.eventDB.map((data) {
+        children: list.map((data) {
           return ListTile(
             title: Text(data.eventName),
             subtitle: Column(
@@ -44,7 +56,14 @@ class _DisplayEvents extends State<DisplayEvents> {
                 Text(data.limitAttending.toString()),
               ],
             ),
-            trailing: FavoriteWidget(),
+            trailing: Icon(
+              data.isFavorite ? Icons.favorite : Icons.favorite_border,
+              color: data.isFavorite ? Colors.red : null,
+            ),
+            onTap: () {
+              data.setFavorite(!data.isFavorite);
+              refresh();
+            },
           );
         }).toList(),
       ),
@@ -52,47 +71,57 @@ class _DisplayEvents extends State<DisplayEvents> {
   }
 }
 
-class FavoriteWidget extends StatefulWidget {
-  _FavoriteWidget createState() => _FavoriteWidget();
-}
+// class FavoriteWidget extends StatefulWidget {
+//   final bool fav;
 
-class _FavoriteWidget extends State<FavoriteWidget> {
-  bool _isFavorited = true;
-  int _favoriteCount = 41;
-  void _toggleFavorite() {
-    setState(() {
-      if (_isFavorited) {
-        _favoriteCount -= 1;
-        _isFavorited = false;
-      } else {
-        _favoriteCount += 1;
-        _isFavorited = true;
-      }
-    });
-  }
+//   const FavoriteWidget({Key key, this.fav}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Container(
-          padding: EdgeInsets.all(0),
-          child: IconButton(
-            padding: EdgeInsets.all(0),
-            alignment: Alignment.centerRight,
-            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
-            color: _isFavorited ? Colors.red : null,
-            onPressed: _toggleFavorite,
-          ),
-        ),
-        SizedBox(
-          width: 18,
-          child: Container(
-            child: Text('$_favoriteCount'),
-          ),
-        ),
-      ],
-    );
-  }
-}
+//   @override
+//   _FavoriteWidget createState() => _FavoriteWidget();
+// }
+
+// class _FavoriteWidget extends State<FavoriteWidget> {
+//   bool _isFavorited;
+//   @override
+//   void initState() {
+//     _isFavorited = widget.fav;
+//   }
+
+//   int _favoriteCount = 41;
+//   void _toggleFavorite() {
+//     setState(() {
+//       if (_isFavorited) {
+//         _favoriteCount -= 1;
+//         _isFavorited = false;
+//       } else {
+//         _favoriteCount += 1;
+//         _isFavorited = true;
+//       }
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Row(
+//       mainAxisSize: MainAxisSize.min,
+//       children: [
+//         Container(
+//           padding: EdgeInsets.all(0),
+//           child: IconButton(
+//             padding: EdgeInsets.all(0),
+//             alignment: Alignment.centerRight,
+//             icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+//             color: _isFavorited ? Colors.red : null,
+//             onPressed: _toggleFavorite,
+//           ),
+//         ),
+//         SizedBox(
+//           width: 18,
+//           child: Container(
+//             child: Text('$_favoriteCount'),
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+// }
